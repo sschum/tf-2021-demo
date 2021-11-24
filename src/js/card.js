3c3fdcfbaefca3fb9eb309f0e578bbce8bc680bf
sschum <s.schumann@tarent.de>
2021-11-24 16:48:41 +0100

    create own card component

---
+++ src/js/card.js
@@ -0,0 +1,11 @@
+Vue.component('card', {
+    template: `
+<div class="card">
+    <div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+    </div>
+</div>
+    `
+})
